56d158740d2e3b952515a47a753230f003cb479b
Reza <mahmudreza1999@gmail.com>
2024-01-15 00:02:00 +0530

    modified a file

--- DSAwithJavaScript/practice.js
+++ DSAwithJavaScript/practice.js
@@ -9,13 +9,15 @@ function addUpTo(n) {
 // function addUpTo(n) {
 //   return n*(n+1)/2;
 // }
-let t1 = performance.now();
+let t1 = performance.now();// performance.now shows us the time when it runs.
 console.log(addUpTo(300));
 let t2 = performance.now();
 console.log(`time took to execute is ${(t2 - t1) / 1000} seconds`);
 /*
 Which code is better?
 -> The code which runs faster and takes less memory space.
+-> The code where there is less or constant number of code operations(additions, subtractions
+    , divisions, multiplications, assignments, comparisions).
 */
 /*
 Advise 1: Always try to find mathematical formulas to make solutions simpler
