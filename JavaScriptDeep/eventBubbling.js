13596614710f66d35edb6f3c6bcf6cd27eeafb53
Reza <mahmudreza1999@gmail.com>
2024-01-22 23:21:42 +0530

    event bubbling

---
+++ JavaScriptDeep/eventBubbling.js
@@ -0,0 +1,7 @@
+const grandfather = document.getElementById('grandfather');
+const father = document.getElementById('father');
+const child = document.getElementById('child');
+// console.log(grandfather,father,child);
+grandfather.addEventListener('click',function(){console.log("grandfather clicked!!!")},false);
+father.addEventListener('click',function(){console.log("father clicked!!!")},false);
+child.addEventListener('click',function(){console.log("child clicked!!!")},false);
