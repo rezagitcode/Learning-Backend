b2c1eaa9cccb7ca38de4ceead1368fc07f7af279
Reza <mahmudreza1999@gmail.com>
2024-01-27 15:59:19 +0530

    learning about environment variables

--- Nodejs/Basics/readfile.js
+++ Nodejs/Basics/readfile.js
@@ -1,7 +1,8 @@
-var http = require('http');
-var fs = require('fs');
+import http from "node:http";
+import fs from "node:fs";
 http.createServer(function (req, res){
-    fs.readFile('file1.html',function(err,data){
+    fs.readFile('file1.txt',function(err,data){
+        if(err) throw err;
         res.writeHead(200,{"content-type":"text/html"});
         res.write(data);
         return res.end();
