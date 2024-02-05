6744dfd95a836153bb8344406935f0626ffeeee4
Reza <mahmudreza1999@gmail.com>
2024-02-05 23:01:20 +0530

    learning

--- Nodejs/Main/app.js
+++ Nodejs/Main/app.js
@@ -1,17 +1,12 @@
 const express = require("express");
 
 const app = express();
-app.use('/',(req, res, next)=>{
-    console.log("Inside first middleware",req.url);
-    next();
+app.use("/",(req, res, next) => {
+  console.log("in the first middleware",req.url);
+  next();
 });
-app.use('/product',(req, res, next)=>{
-    console.log("Inside product",req.url);
-    res.send(`<h1>This is product page</h1>`);
-});
-app.use('/',(req, res, next)=>{
-    console.log("Inside another middleware",req.url);
-    res.send(`<h1>Salam from express.js</h1>`);
+app.use("/dr",(req, res, next) => {
+  console.log("in the another middleware",req.url);
 });
 
 app.listen(3000);
