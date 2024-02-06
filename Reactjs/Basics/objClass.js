f09792c36304b4a7bc4a299c6d35e4c7b1cff8c8
Reza <mahmudreza1999@gmail.com>
2024-02-06 23:44:31 +0530

    React

---
+++ Reactjs/Basics/objClass.js
@@ -0,0 +1,9 @@
+const user = {
+    username:'Reza',
+    userAge:24,
+    greet(){
+        console.log("Hello!!");
+    }
+
+}
+console.log(user.greet());
---
+++ Reactjs/Basics/practice.js
@@ -0,0 +1,6 @@
+const password = prompt("type password");
+if(password === "Reza"){
+    console.log("works");
+}else{
+    console.log("doesnot work");
+}
