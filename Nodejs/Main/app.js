9b3dcc656aec219834d7a8b506a8dd8ee99c0c42
Reza <mahmudreza1999@gmail.com>
2024-02-09 23:42:10 +0530

    Learning Nodejs

--- Nodejs/Main/app.js
+++ Nodejs/Main/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
 const app = express();
-app.use("/",(req, res, next) => {
-  console.log("in the first middleware",req.url);
-  next();
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+app.use("/users",(req, res, next) => {
+  console.log("Inside users middleware");
+  res.send(`<h1>Users</h1><ul><li>User1</li><li>User2</li><li>User2</li></ul>`);
 });
-app.use("/dr",(req, res, next) => {
-  console.log("in the another middleware",req.url);
+app.use("/",(req, res, next) => {
+  console.log("Inside / ");
+  res.end(`<h1>My Homepage</h1>`);
 });
 
 app.listen(3000);
