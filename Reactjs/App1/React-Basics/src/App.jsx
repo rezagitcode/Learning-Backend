a5b94539553e6a95090ca6599ccac5aff2f0c45f
Reza <mahmudreza1999@gmail.com>
2024-02-10 22:43:32 +0530

    Revising React Concepts

--- Reactjs/App1/React-Basics/src/App.jsx
+++ Reactjs/App1/React-Basics/src/App.jsx
@@ -1,43 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-const myWord = ['React','Html','JavaScript','CSS','Tailwind'];
-function genRandomWord(){
-  return (Math.floor(Math.random()*4 +1))
+import reactImage from './assets/react-core-concepts.png';
+import {CORE_CONCEPTS} from './data.js';
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
-function Header(){
-  const myWords = myWord[genRandomWord()];
-  return (<>
-  <header>
-  <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>{myWords} is Love</h1>
-      </header>
-  </>);
+
+function Header() {
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+function CoreConcept({image, title, description}){
+  return (<li>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>)
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Header/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Header />
+      <main>
+        <section id='core-concepts'>
+        <h2>Core Concepts</h2>
+        <ul>
+         <CoreConcept image={CORE_CONCEPTS[0].image}
+         title={CORE_CONCEPTS[0].title}
+         description={CORE_CONCEPTS[0].description}/>
+         <CoreConcept {...CORE_CONCEPTS[1]}/>
+         <CoreConcept {...CORE_CONCEPTS[2]}/>
+         <CoreConcept {...CORE_CONCEPTS[3]}/>
+        </ul>
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
