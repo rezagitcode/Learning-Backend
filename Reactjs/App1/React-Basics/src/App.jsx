e977ee447c4bcc4b87a94daa05939742c42de0de
Reza <mahmudreza1999@gmail.com>
2024-02-16 23:58:23 +0530

    changing CSS

--- Reactjs/App1/React-Basics/src/App.jsx
+++ Reactjs/App1/React-Basics/src/App.jsx
@@ -1,5 +1,6 @@
 import CoreConcept from "./components/CoreConcept.jsx";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 
 function App() {
@@ -19,6 +20,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
         </section>
       </main>
     </div>
